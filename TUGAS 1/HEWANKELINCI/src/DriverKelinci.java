e220d17aee34d311f37e13245f76ce0840691e22
Dikameo <idikach30@gmail.com>
2024-03-04 15:09:31 +0700

    Perubahan atribut

--- TUGAS 1/HEWANKELINCI/src/DriverKelinci.java
+++ TUGAS 1/HEWANKELINCI/src/DriverKelinci.java
@@ -2,6 +2,11 @@ public class DriverKelinci {
     public static void main(String[] args) {
         Kelinci hewanku = new Kelinci();
         hewanku.show();
+        System.out.println("==================================");
         hewanku.aktivitas();
+        System.out.println("==================================");
+        hewanku.cekJenisKelamin();
+        System.out.println("==================================");
+        hewanku.cekMakananFav();
     }
 }
--- TUGAS 1/HEWANKELINCI/src/Kelinci.java
+++ TUGAS 1/HEWANKELINCI/src/Kelinci.java
@@ -5,14 +5,11 @@ public class Kelinci {
     public String makanan = "Wortel";
     public String warna = "Putih";
     public int jmlanak = 10;
-
-    Kelinci(){
-
-    }
+    public String kegiatan = "Rebahan";
 
     void show(){
         System.out.println("Nama Kelinciku : "+nama);
-        System.out.println("Umur Kelinciku : "+umur);
+        System.out.println("Umur Kelinciku : "+umur+ " Bulan");
         System.out.println("Jenis Kelamin Kelinciku : "+jeniskelamin);
         System.out.println("Makanan Favorite Kelinciku : "+makanan);
         System.out.println("Warna Kelinciku : "+warna);
@@ -20,6 +17,13 @@ public class Kelinci {
     }
 
     void aktivitas(){
-        System.out.println("Setiap hari "+ nama + " kelinciku selaluku ajak bermain");
+        System.out.println("Setiap hari "+ nama + " memiliki hobby " + kegiatan);
+    }
+    void cekJenisKelamin(){
+        System.out.println("Jenis Kelaminku "+ jeniskelamin);
+    }
+
+    void cekMakananFav(){
+        System.out.println("Makanan Favoritku "+ makanan );
     }
 }
